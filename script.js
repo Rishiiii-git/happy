73ce802d65fe5df6b06f4509d52884bbd2c69c52
Rishiiii-git <flashshorts16@gmail.com>
2025-05-08 20:19:38 +0545

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+function showSurprise() {
+    const surpriseMessage = document.getElementById('surprise-message');
+    surpriseMessage.style.display = 'block';
+    surpriseMessage.classList.add('fadeInSurprise');
+}
